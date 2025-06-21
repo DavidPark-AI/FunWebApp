0c4b08c3c1823a547fa10e22250e6b0a1593afb9
David <liquidpsm@gmail.com>
2025-06-21 20:44:43 +0900

    애드센스광고 확인

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-3228294204750660" />
+      </head>
       <body className={inter.className}>
         <main className="min-h-screen flex flex-col">
           {children}
